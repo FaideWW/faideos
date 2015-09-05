b40e4ec1cfc4dbf8d2eefd5dcdbb34c81dfb470e
FaideWW <michael@faide.net>
2015-09-05 13:11:15 -0600

    Vertical scrolling and VGA color text

--- kernel.c
+++ kernel.c
@@ -34,6 +34,8 @@ enum vga_color {
     COLOR_WHITE = 15,
 };
 
+static const size_t NUMCOLORS = 16;
+
 uint8_t make_color(enum vga_color fg, enum vga_color bg) {
     return fg | bg << 4;
 }
@@ -83,19 +85,42 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
     terminal_buffer[index] = make_vgaentry(c, color);
 }
 
+void terminal_clearrow(size_t row) {
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+        terminal_putentryat(' ', terminal_color, x, row);
+    }
+}
+
+void terminal_clearbottomrow() {
+    terminal_clearrow(VGA_HEIGHT);
+}
+
+void terminal_scrolldown() {
+    for (size_t y = 1; y <= VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            size_t old_index = y * VGA_WIDTH + x;
+            size_t new_index = (y - 1) * VGA_WIDTH + x;
+            terminal_buffer[new_index] = terminal_buffer[old_index];
+            terminal_clearbottomrow();
+        }
+    }
+}
+
 void terminal_putchar(char c) {
     if (c == '\n') {
         terminal_column = 0;
         if (++terminal_row == VGA_HEIGHT) {
-            terminal_row = 0;
+            terminal_row--;
+            terminal_scrolldown();
         }
-        return;
-    }
-    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    if (++terminal_column == VGA_WIDTH) {
-        terminal_column = 0;
-        if (++terminal_row == VGA_HEIGHT) {
-            terminal_row = 0;
+    } else {
+        terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+        if (++terminal_column == VGA_WIDTH) {
+            terminal_column = 0;
+            if (++terminal_row == VGA_HEIGHT) {
+                terminal_row--;
+                terminal_scrolldown();
+            }
         }
     }
 }
@@ -103,6 +128,14 @@ void terminal_putchar(char c) {
 void terminal_writestring(const char *data) {
     size_t datalen = strlen(data);
     for (size_t i = 0; i < datalen; i++) {
+        terminal_putchar(data[i]);
+    }
+}
+
+void terminal_writerainbow(const char *data) {
+    size_t datalen = strlen(data);
+    for (size_t i = 0; i < datalen; i++) {
+        terminal_setcolor(++terminal_color % NUMCOLORS);
         terminal_putchar(data[i]);
     }
 }
@@ -115,8 +148,11 @@ void kernel_main() {
     /* Initialize terminal interface */
     terminal_initialize();
 
-    /*
-    No support for newlines yet
-    */
-    terminal_writestring("Hello, kernel World!\nThis is a second line!");
+    terminal_writestring("You shouldn't see this string if scrolling works...\n");
+    for (size_t row = 0; row < VGA_HEIGHT - 1; row++) {
+        terminal_writerainbow("Hello kernel world!\n");
+    }
+    terminal_setcolor(COLOR_LIGHT_GREY);
+    terminal_writestring("Reached bottom of terminal");
+
 }
