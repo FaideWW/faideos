8fa26eb481889b176e3d1fd5421ee8a6e8ba0f6c
FaideWW <michael@faide.net>
2015-09-05 12:31:48 -0600

    Newline support

--- kernel.c
+++ kernel.c
@@ -8,7 +8,6 @@
 #if defined(__linux__)
 #error "You are not using a cross compiler, trouble awaits"
 #endif
-xxx
 
 #if !defined(__i386__)
 #error "Your compiler target is wrong; use ix86 compiler"
@@ -85,6 +84,13 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putchar(char c) {
+    if (c == '\n') {
+        terminal_column = 0;
+        if (++terminal_row == VGA_HEIGHT) {
+            terminal_row = 0;
+        }
+        return;
+    }
     terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
     if (++terminal_column == VGA_WIDTH) {
         terminal_column = 0;
